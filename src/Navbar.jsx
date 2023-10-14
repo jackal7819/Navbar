91b4600f444b123e9d04689de1f02d4b880dc696
Viktor Filippov <jackal3166@gmail.com>
2023-10-15 02:07:19 +0300

    Add adaptive

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -37,6 +37,13 @@ const Navbar = () => {
 						))}
 					</ul>
 				</div>
+				<ul className='social-icons'>
+						{social.map((icon) => (
+							<li key={icon.id}>
+								<a href={icon.url}>{icon.icon}</a>
+							</li>
+						))}
+					</ul>
 			</div>
 		</nav>
 	);
